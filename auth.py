4146acd7482a0ded1b22bdf4186861744b409265
Lucas <lucas98delima@gmail.com>
2026-09-01 17:47:15 -0300

    fix authentication get user

--- auth.py
+++ auth.py
@@ -1,8 +1,8 @@
-from extensions import login_manager
+from extensions import login_manager, db
 from models import User
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    return db.session.get(User,user_id)
 
 login_manager.login_view="/"
