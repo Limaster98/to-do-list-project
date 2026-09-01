b3be50c88f15fbff98c54344e447dd37a0a60438
Lucas <lucas98delima@gmail.com>
2026-09-01 13:20:33 -0300

    add: insert db for development

--- application.py
+++ application.py
@@ -19,5 +19,8 @@ def register():
 def dashboard():
     return render_template("dashboard.html")
 
+with application.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
     application.run(debug=True)
