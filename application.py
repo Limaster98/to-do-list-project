c501c239e797d7e892017cae27d2d0c37d9f08a7
Lucas <lucas98delima@gmail.com>
2026-09-01 17:47:50 -0300

    add login and register routes

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from extensions import db, login_manager
-from flask_login import login_required
+from flask_login import login_required, login_user
+from models import User
 import auth
 
 application = Flask(__name__)
@@ -11,10 +12,32 @@ login_manager.init_app(application)
 
 @application.route("/", methods=["POST","GET"])
 def home():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username and password:
+            user = User.query.filter_by(username=username, password=password).first()
+            if user:
+                login_user(user)
+            
+            return redirect(url_for('dashboard'))
+        
     return render_template("index.html")
 
 @application.route("/register", methods=["POST","GET"])
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username and password:
+            new_user = User(username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            
+            return redirect(url_for('dashboard'))
+
     return render_template("register.html")
 
 @application.route("/dashboard", methods=["POST","GET"])
